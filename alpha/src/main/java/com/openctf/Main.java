ba44887a543ed170632cf1f2f6293b4233d3670c
averyB <averwhy@gmail.com>
2023-04-01 13:58:40 -0400

    Initial commit

---
+++ alpha/src/main/java/com/openctf/Main.java
@@ -0,0 +1,10 @@
+package com.openctf;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
